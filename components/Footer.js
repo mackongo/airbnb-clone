62abfd4c80de1db5df810e1c498adc6c04148c84
mackongo <mackongo@gmail.com>
2021-08-04 21:37:59 +0300

    day 1 challenge

---
+++ components/Footer.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="text-center grid grid-cols-1 md:grid-cols-5 gap-y-10 px-32 py-14 bg-gray-100">
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>News Room</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+       <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It's a pretty awesome clone</p>
+        <p>Referrals Accepted</p>
+        <p>Ally Makongo</p>
+      </div>
+
+        <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Youtube</p>
+        <p>Presents</p>
+        <p>PapaReact</p>
+        <p>Awesome Build</p>
+        <p>Join Now</p>
+      </div>
+
+       <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">POLICY</h5>
+        <p>Users Acceptance</p>
+        <p>6 Figures Salaries</p>
+        <p>Podcast</p>
+        <p>Developer Journey</p>
+        <p>Zero to Hero</p>
+      </div>
+
+        <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>News Room</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
